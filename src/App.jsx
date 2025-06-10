bd9150ae450fe4530a382df0880ae2df94895e2d
Shaurya Sharma <shaurya1.sharma@one97.delhi.net>
2025-06-10 17:10:00 +0530

    Bas karo

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Home'
 import Contact from './Contact'
 import About from './About'
 import CT from './CT'
 import Login from './components/Login'
+import Signin from './components/Signin'
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contacts" element={<Contact />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/creating-team" element={<CT />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contacts" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/creating-team" element={<CT />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signin />} />
+      </Routes>
     </BrowserRouter>
   )
 }
